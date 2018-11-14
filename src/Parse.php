1e1120109f6df60b4adb98941bb55455f2337344
gogl92 <luisarmando1234@gmail.com>
2018-11-13 23:24:36 -0600

    update

---
+++ src/Parse.php
@@ -0,0 +1,27 @@
+<?php
+namespace inquid\parse;
+
+use yii\base\Component;
+
+class TermicTicket extends Component
+{
+    public $app_id;
+    public $rest_key;
+    public $master_key;
+    public $server_url;
+    public $port = 1337;
+
+    public function init()
+    {
+        parent::init();
+        ParseClient::initialize( $this->app_id, $this->rest_key, $this->master_key );
+        ParseClient::setServerURL("{$this->server_url}:{$this->port}",'parse');
+
+    }
+
+    public function healthCheck(){
+      $health = ParseClient::getServerHealth();
+      if($health['status'] === 200) ? return true:return false;
+    }
+
+}
